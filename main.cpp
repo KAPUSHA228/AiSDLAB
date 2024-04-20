744083ef70b5ec1c98fbacf23e2b85d8af3fc108
egorm <egormorozov123450@gmail.com>
2024-04-20 22:16:25 +0300

    fix: print all tokens

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <regex>
 #include<string>
-
+#include "Expression/Expression.h"
 #include "Lexer.h"
 
 using namespace std;
+
+void forTestRegex(const string &str);
 
 int main() {
     /* PascalABC for example
@@ -15,6 +17,8 @@ int main() {
      *b := 13;
      *end.
      */
+
+    //В конце программы - end.   !!!
     string text_program_pascal =
             "var a:real;\n"
             "var b:char;\n"
@@ -23,17 +27,21 @@ int main() {
             "   a := 1.2;\n"
             "   b := '9';\n"
             "\tc := a + b;\n"
-            "end;\n";
+            "end.\n";
     // cout<<text_program_pascal;
+    forTestRegex(" +dshgdhgads)");
 
-    const std::string s = " +dshgdhgads)";
+    Lexer lexer(text_program_pascal);
+    Expression ex();
+    return 0;
+}
+
+void forTestRegex(const string &str) {
+    const std::string &s = str;
     std::regex rgx("^[ \n\t]");
     std::smatch match;
     if (std::regex_search(s.begin(), s.end(), match, rgx)) {
         string str2 = static_cast<string>(match[0]);
         // cout << str2.length()<<endl;
     }
-    Lexer lexer(text_program_pascal);
-
-    return 0;
 }
